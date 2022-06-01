84f1598a4334b93926e81890c08d6c7a55edcdae
Vatsal-24 <vatsaldoshi11@gmail.com>
2022-06-01 22:17:22 +0530

    hello world

--- front-end/src/components/Main.js
+++ front-end/src/components/Main.js
@@ -12,17 +12,31 @@ import { TableBody } from "@mui/material";
 
 function Main() {
   const [coins, setCoins] = useState([]);
+  const [gainers, setGainers] = useState([]);
+  const [loosers, setLoosers] = useState([]);
   const [search, setSearch] = useState("");
 
   useEffect(() => {
     // setInterval(() => {
     axios
       .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
       )
       .then((response) => {
         setCoins(response.data);
         console.log(response.data);
+        let pos = [],
+          neg = [];
+        response.data.map((ele) => {
+          if (ele.price_change_24h > 0) {
+            pos.push(ele);
+          } else {
+            neg.push(ele);
+          }
+        });
+        console.log("pos", pos, "neg", neg);
+        setGainers(pos);
+        setLoosers(neg);
       })
       .catch((error) => {
         console.log(error);
@@ -40,7 +54,7 @@ function Main() {
 
   return (
     <Grid container justifyContent="center">
-      <Grid item textAlign="center">
+      <Grid item textAlign="center" xs={12}>
         <h1>Search a currency</h1>
         <TextField
           id="outlined-search"
@@ -50,14 +64,13 @@ function Main() {
           value={search}
         />
       </Grid>
-      <Grid item xs={12}>
+      <Grid item xs={8}>
         <TableContainer component={Paper}>
           <Table aria-label="simple table">
             <TableHead>
               <TableRow>
                 <TableCell align="center">Logo</TableCell>
                 <TableCell align="left">Name</TableCell>
-                <TableCell align="left">Symbol</TableCell>
                 <TableCell align="left">CMP</TableCell>
                 <TableCell align="left">Volume</TableCell>
                 <TableCell align="left">%change</TableCell>
@@ -75,9 +88,6 @@ function Main() {
                     <p>{coin.name}</p>
                   </TableCell>
                   <TableCell>
-                    <p>{coin.symbol}</p>
-                  </TableCell>
-                  <TableCell>
                     <p>${coin.current_price}</p>
                   </TableCell>
                   <TableCell>
@@ -85,7 +95,7 @@ function Main() {
                   </TableCell>
                   <TableCell>
                     {coin.price_change_24h < 0 ? (
-                      <p>{coin.price_change_24h.toFixed(2)}%</p>
+                      <p>{coin.price_change_24h}%</p>
                     ) : (
                       <p>{coin.price_change_24h.toFixed(2)}%</p>
                     )}
@@ -101,6 +111,72 @@ function Main() {
             </TableBody>
           </Table>
         </TableContainer>
+      </Grid>
+      <Grid item container xs={4} style={{ maxHeight: "20px" }}>
+        <Grid item xs={12}>
+          <TableContainer component={Paper}>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center">Logo</TableCell>
+                  <TableCell align="left">Name</TableCell>
+                  <TableCell align="left">%change</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {gainers.map((coin) => (
+                  <TableRow key={coin.name}>
+                    <TableCell align="center">
+                      <img src={coin.image} alt="coin" width="30%" />
+                    </TableCell>
+                    <TableCell>
+                      <p>{coin.name}</p>
+                    </TableCell>
+                    <TableCell>
+                      {coin.price_change_24h < 0 ? (
+                        <p>{coin.price_change_24h.toFixed(2)}%</p>
+                      ) : (
+                        <p>{coin.price_change_24h.toFixed(2)}%</p>
+                      )}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+        <Grid item xs={12}>
+          <TableContainer component={Paper}>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center">Logo</TableCell>
+                  <TableCell align="left">Name</TableCell>
+                  <TableCell align="left">%change</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {loosers.map((coin) => (
+                  <TableRow key={coin.name}>
+                    <TableCell align="center">
+                      <img src={coin.image} alt="coin" width="30%" />
+                    </TableCell>
+                    <TableCell>
+                      <p>{coin.name}</p>
+                    </TableCell>
+                    <TableCell>
+                      {coin.price_change_24h < 0 ? (
+                        <p>{coin.price_change_24h.toFixed(2)}%</p>
+                      ) : (
+                        <p>{coin.price_change_24h.toFixed(2)}%</p>
+                      )}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
       </Grid>
     </Grid>
   );
